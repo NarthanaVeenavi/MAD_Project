e8f47540c25db7da2d8b0632f4c6c58e7bb3cf51
realdishi <72241651+realdishi@users.noreply.github.com>
2021-05-06 14:19:41 +0530

    Added the other UI's for the Patient function. Added the intents in the patient dashbord. Minor changes in the home UI for the Patient function.

--- app/src/main/java/com/example/home/PatientHome.java
+++ app/src/main/java/com/example/home/PatientHome.java
@@ -2,7 +2,9 @@ package com.example.home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PatientHome extends AppCompatActivity {
 
@@ -11,4 +13,20 @@ public class PatientHome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_home);
     }
+
+    public void newReq(View view){
+        Intent i = new Intent(this, newReq.class);
+        startActivity(i);
+       }
+
+
+    public void openReq(View view){
+        Intent i = new Intent(this, reqPage.class);
+        startActivity(i);
+       }
+
+    public void openRes(View view){
+        Intent i = new Intent(this, donorResPage.class);
+        startActivity(i);
+        }
 }
---
+++ app/src/main/java/com/example/home/donorResPage.java
@@ -0,0 +1,14 @@
+package com.example.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class donorResPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donor_res_page);
+    }
+}
